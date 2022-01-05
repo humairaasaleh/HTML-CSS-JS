7e9e5e2af96db2295f8a934abebdd8a0c010017b
Humairaa Saleh <humairaasaleh@gmail.com>
2022-01-05 16:22:40 +0000

    add files npm

--- Node/index.js
+++ Node/index.js
@@ -1,3 +1,12 @@
-const {greetingString, greetingPrint} = require("./greeting");
-console.log(greetingString);
-greetingPrint();
+// const {greetingString, greetingPrint} = require("./greeting");
+// console.log(greetingString);
+// greetingPrint();
+
+const snakeNames= require('snake-names');
+
+console.log(snakeNames.random());
+
+const { getCatAgeObject, getCatAgeString } = require('cat-age-calculator/src');
+console.log(getCatAgeObject(12));
+
+console.log(getCatAgeString(22));
